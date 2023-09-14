efa1e0b0aae666c98a397f0786790ecf775695a1
HeinrichLeandro <heinrich.leandro@gmail.com>
2023-09-14 19:54:43 -0300

    calculadora totalmente funcionando con todas las funciones y acomodada la pantalla

--- index.js
+++ index.js
@@ -30,8 +30,45 @@ botones.forEach(boton => {
             return;
         }
 
+        if (boton.id === "porc") {
+            try {
+                pantalla.textContent = eval(pantalla.textContent)/"100";
+            } catch {
+                pantalla.textContent = "Error!";
+            }
+            return;
+        }
+
+        if (boton.id === "invertir") {
+            try {
+                pantalla.textContent = "1"/eval(pantalla.textContent);
+            } catch {
+                pantalla.textContent = "Error!";
+            }
+            return;
+        }
+
         if (boton.id === "coma"){
             pantalla.textContent += botonApretado;
+            return;
+        }
+
+        if (boton.id === "cambia") {
+            if (pantalla.textContent === 0 || pantalla.textContent === "Error!") {
+                pantalla.textContent = "0";
+            } else {
+                pantalla.textContent = "-1"*pantalla.textContent;
+            }
+            return;
+        }
+
+        if (boton.id === "cuadratica") {
+            pantalla.textContent = pantalla.textContent*pantalla.textContent;
+            return;
+        }
+
+        if (boton.id === "raiz") {
+            pantalla.textContent = Math. sqrt(pantalla.textContent);
             return;
         }
         
